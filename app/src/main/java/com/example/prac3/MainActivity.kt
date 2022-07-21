7a38c03bbf168e00f8e04db79494f898169ed25b
chowminxuan <serlynchow@gmail.com>
2022-07-21 17:06:56 +0800

    init commit

---
+++ app/src/main/java/com/example/prac3/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.prac3
+
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.prac3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    //Student s = new Student();
+    //var s: Student
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       binding = DataBindingUtil.setContentView(this,
+        R.layout.activity_main)
+
+        var student = Student("Ali","RIT",20)
+
+       binding.btnSetValue.setOnClickListener(){
+           student.age=28
+         binding.apply{
+             invalidateAll() //call to refresh
+         }
+
+       }
+        binding.btnGetValue.setOnClickListener(){
+
+            binding.myData=student
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/prac3/Student.kt
@@ -0,0 +1,8 @@
+package com.example.prac3
+
+data class Student(var name:String="",
+                   var programme:String="",
+                   var age:Int=0) //in kotlin Int is uppercase
+
+
+
